a427b7ba7d2f3c39d7b9327d607ec53a94f81b99
Jade Lovelace <software@lfcode.ca>
2023-08-22 20:35:33 -0700

    yeet

---
+++ flake.nix
@@ -0,0 +1,13 @@
+{
+  description = "A very basic flake";
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/313dcfae7e6a0c11f305ba066c83d25c78744333";
+
+  outputs = { self, nixpkgs }:
+    let pkgs = import nixpkgs { system = "x86_64-linux"; }; in
+    {
+      packages.x86_64-linux.default = pkgs.runCommand "hm" {} ''
+        echo NYA NYA
+        touch $out
+      '';
+    };
+}
